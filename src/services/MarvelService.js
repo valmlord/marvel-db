a5a4e8b7b0627c20b4ed5f0971d8d54f4d8a0d17
valmlord <korshukovstepan@gmail.com>
2022-06-19 23:20:22 +0300

    add random character function

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,9 @@
 
 
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=0ba8221b6777712416f94e652d53a0e6';
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -11,9 +14,25 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=0ba8221b6777712416f94e652d53a0e6');
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
-
+ 
 export default MarvelService;
