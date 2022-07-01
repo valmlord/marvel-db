9fb20dd0fe9b4d0d4f0503d2e3c34079f81b2dc7
valmlord <korshukovstepan@gmail.com>
2022-07-01 09:56:40 +0300

    complete functional of first page

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from "prop-types";
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -126,4 +127,8 @@ const View = ({char}) => {
     )
 }
 
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
 export default CharInfo;
